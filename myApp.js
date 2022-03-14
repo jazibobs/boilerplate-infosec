7d579934fef91839b4d41062427850ba51211492
Jared Rigby <jared@mtddigital.uk>
2022-03-14 19:30:52 +0800

    Added frameguard protection using Helmet

--- myApp.js
+++ myApp.js
@@ -2,8 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy())
+// Hide express headers
+app.use(helmet.hidePoweredBy());
 
+// Disable iframe usage
+app.use(helmet.frameguard({action: 'deny'}));
 
 
 
